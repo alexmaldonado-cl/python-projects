832bcae5deca665a18def308a06091a7c9b3bdc5
Alex Maldonado <alex.maldonado@outlook.com>
2023-02-18 21:56:29 -0300

    Added word dictionary

---
+++ 14_word_dictionary.py
@@ -0,0 +1,21 @@
+
+def main():
+    word_dictionary = {
+        'hi': 'a way of gretting',
+        'eyes': 'an organ for seeing',
+        'earth': 'a planet in space',
+    }
+
+    while True:
+
+        word = input("Enter a word: ").lower()
+
+        if word == "":
+            break
+
+        if word in word_dictionary:
+            print(word, ":", word_dictionary[word])
+
+
+if __name__ == "__main__":
+    main()
